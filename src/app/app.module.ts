492eb1bfbb59c0c20a6ad4947883154951a94688
drhansen000 <drhansen000@hotmail.com>
2019-07-16 15:50:11 -0600

    Created basic structure for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './common/top-nav/top-nav.component';
+import { HomeComponent } from './home/home.component';
+import { MealListComponent } from './meals/meal-list/meal-list.component';
+import { MealDetailComponent } from './meals/meal-detail/meal-detail.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { OrderDetailComponent } from './orders/order-detail/order-detail.component';
+import { StarredDetailComponent } from './home/starred-detail/starred-detail.component';
+import { RegisterComponent } from './account/register/register.component';
+import { LoginComponent } from './account/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    HomeComponent,
+    MealListComponent,
+    MealDetailComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    OrderListComponent,
+    OrderDetailComponent,
+    StarredDetailComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/starred-detail/starred-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starred-detail',
+  templateUrl: './starred-detail.component.html',
+  styleUrls: ['./starred-detail.component.css']
+})
+export class StarredDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
